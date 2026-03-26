a41c648af422f200cab02a8553fd23535164cf42
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:28:03 +0400

    Update Проект 6,8.2.cs

--- Проект 6,8.2.cs
+++ Проект 6,8.2.cs
@@ -1,28 +1,26 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите x: ");
+        double x = double.Parse(Console.ReadLine());
+        Console.Write("Введите n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double[] массив = new double[n];
+        double степень = x;
+        for (int i = 1; i <= n; i++)
         {
-            Console.Write("Введите x: ");
-            double x = double.Parse(Console.ReadLine());
-            Console.Write("Введите n: ");
-            int n = int.Parse(Console.ReadLine());
-            
-            double[] массив = new double[n];
-            double степень = x;          
-            for (int i = 1; i <= n; i++)
-            {
-                массив[i - 1] = степень / i;
-                степень *= x * x;
-            }       
-            double сумма = 0;
-            foreach (double элемент in массив)
-            {
-                сумма += элемент;
-            }     
-            Console.WriteLine($"Сумма = {сумма:F4}");
-            Console.ReadKey();
+            массив[i - 1] = степень / i;
+            степень *= x * x;
         }
+        double сумма = 0;
+        foreach (double элемент in массив)
+        {
+            сумма += элемент;
+        }
+        Console.WriteLine($"Сумма = {сумма:F4}");
+        Console.ReadKey();
     }
 }
