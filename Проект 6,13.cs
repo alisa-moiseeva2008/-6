99b466f0298bd10f3a4389a86f7171602bb85a39
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:16:15 +0400

    Update Проект 6,13.cs

--- Проект 6,13.cs
+++ Проект 6,13.cs
@@ -1,29 +1,27 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите длину массива n: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] массив = new int[n];
+
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("Введите длину массива n: ");
-            int n = int.Parse(Console.ReadLine());
-            int[] массив = new int[n];
-            
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            }         
-            int[] квадраты = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                квадраты[i] = массив[i] * массив[i];
-            }   
-            Console.WriteLine("Квадраты элементов:");
-            foreach (int элемент in квадраты)
-            {
-                Console.Write(элемент + " ");
-            }
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        int[] квадраты = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            квадраты[i] = массив[i] * массив[i];
+        }
+        Console.WriteLine("Квадраты элементов:");
+        foreach (int элемент in квадраты)
+        {
+            Console.Write(элемент + " ");
+        }
+        Console.ReadKey();
     }
 }
