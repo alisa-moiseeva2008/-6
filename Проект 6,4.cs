49c139cb71e0f2a5336be7fd67d31fc3e344582a
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:25:35 +0400

    Update Проект 6,4.cs

--- Проект 6,4.cs
+++ Проект 6,4.cs
@@ -1,29 +1,27 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[] массив = new int[17];
+        for (int i = 0; i < массив.Length; i++)
         {
-            int[] массив = new int[17];           
-            for (int i = 0; i < массив.Length; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            }          
-            int сумма = 0;
-            int количество = 0;
-            
-            foreach (int элемент in массив)
-            {
-                if (элемент > 0 && элемент % 2 != 0)
-                {
-                    сумма += элемент;
-                    количество++;
-                }
-            }        
-            double среднее = (double)сумма / количество;
-            Console.WriteLine($"Среднее значение нечетных положительных элементов: {среднее:F2}");
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        int сумма = 0;
+        int количество = 0;
+
+        foreach (int элемент in массив)
+        {
+            if (элемент > 0 && элемент % 2 != 0)
+            {
+                сумма += элемент;
+                количество++;
+            }
+        }
+        double среднее = (double)сумма / количество;
+        Console.WriteLine($"Среднее значение нечетных положительных элементов: {среднее:F2}");
+        Console.ReadKey();
     }
 }
