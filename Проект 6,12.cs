20fedc9bbf556f90b96ef4cb62c41679295fc39c
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:15:38 +0400

    Update Проект 6,12.cs

--- Проект 6,12.cs
+++ Проект 6,12.cs
@@ -1,37 +1,35 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите длину массива n: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] массив = new int[n];
+
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("Введите длину массива n: ");
-            int n = int.Parse(Console.ReadLine());         
-            int[] массив = new int[n];
- 
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            }
-            int[] новыйМассив = new int[n];
-            
-            for (int i = 0; i < n; i++)
-            {
-                if (массив[i] % 2 != 0)
-                {
-                    новыйМассив[i] = массив[i] + 1;
-                }
-                else
-                {
-                    новыйМассив[i] = массив[i];
-                }
-            }
-            Console.WriteLine("Новый массив (все элементы четные):");
-            foreach (int элемент in новыйМассив)
-            {
-                Console.Write(элемент + " ");
-            }
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        int[] новыйМассив = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (массив[i] % 2 != 0)
+            {
+                новыйМассив[i] = массив[i] + 1;
+            }
+            else
+            {
+                новыйМассив[i] = массив[i];
+            }
+        }
+        Console.WriteLine("Новый массив (все элементы четные):");
+        foreach (int элемент in новыйМассив)
+        {
+            Console.Write(элемент + " ");
+        }
+        Console.ReadKey();
     }
 }
