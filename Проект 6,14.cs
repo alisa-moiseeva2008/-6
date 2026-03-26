42ac94246d69ffe7b7e87783000e264e518ac639
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:16:47 +0400

    Update Проект 6,14.cs

--- Проект 6,14.cs
+++ Проект 6,14.cs
@@ -1,29 +1,27 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите длину массива n (n > 4): ");
+        int n = int.Parse(Console.ReadLine());
+        int[] массив = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("Введите длину массива n (n > 4): ");
-            int n = int.Parse(Console.ReadLine());           
-            int[] массив = new int[n];          
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            }         
-            int[] новыйМассив = new int[n - 4];
-            
-            for (int i = 4; i < n; i++)
-            {
-                новыйМассив[i - 4] = массив[i];
-            }         
-            Console.WriteLine("Новый массив (без первых 4 элементов):");
-            foreach (int элемент in новыйМассив)
-            {
-                Console.Write(элемент + " ");
-            }
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        int[] новыйМассив = new int[n - 4];
+
+        for (int i = 4; i < n; i++)
+        {
+            новыйМассив[i - 4] = массив[i];
+        }
+        Console.WriteLine("Новый массив (без первых 4 элементов):");
+        foreach (int элемент in новыйМассив)
+        {
+            Console.Write(элемент + " ");
+        }
+        Console.ReadKey();
     }
 }
