f710803078ae839f8befdb20d868da217b68b663
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:18:03 +0400

    Update Проект 6,17.cs

--- Проект 6,17.cs
+++ Проект 6,17.cs
@@ -1,39 +1,37 @@
 using System;
 using System.Collections.Generic;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите длину массива n: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] массив = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("Введите длину массива n: ");
-            int n = int.Parse(Console.ReadLine());        
-            int[] массив = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            }
-            List<int> новыйСписок = new List<int>();
-            
-            foreach (int элемент in массив)
-            {
-                if (элемент < 20)
-                {
-                    новыйСписок.Add(элемент);
-                }
-                else
-                {
-                    break;
-                }
-            }
-            int[] новыйМассив = новыйСписок.ToArray();
-            
-            Console.WriteLine("Новый массив (элементы < 20 из начала):");
-            foreach (int элемент in новыйМассив)
-            {
-                Console.Write(элемент + " ");
-            }
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        List<int> новыйСписок = new List<int>();
+
+        foreach (int элемент in массив)
+        {
+            if (элемент < 20)
+            {
+                новыйСписок.Add(элемент);
+            }
+            else
+            {
+                break;
+            }
+        }
+        int[] новыйМассив = новыйСписок.ToArray();
+
+        Console.WriteLine("Новый массив (элементы < 20 из начала):");
+        foreach (int элемент in новыйМассив)
+        {
+            Console.Write(элемент + " ");
+        }
+        Console.ReadKey();
     }
 }
