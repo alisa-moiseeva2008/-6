462a3a084d970e0352ec433c838bfa4ee0bf45a9
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:14:42 +0400

    Update Проект 6,10.cs

--- Проект 6,10.cs
+++ Проект 6,10.cs
@@ -1,28 +1,26 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите длину массива n: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] массив = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("Введите длину массива n: ");
-            int n = int.Parse(Console.ReadLine());     
-            int[] массив = new int[n];          
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            }     
-            bool естьОтрицательный = false;
-            foreach (int элемент in массив)
-            {
-                if (элемент < 0)
-                {
-                    естьОтрицательный = true;
-                    break;
-                }
-            }   
-            Console.WriteLine($"Есть хотя бы один отрицательный: {естьОтрицательный}");
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        bool естьОтрицательный = false;
+        foreach (int элемент in массив)
+        {
+            if (элемент < 0)
+            {
+                естьОтрицательный = true;
+                break;
+            }
+        }
+        Console.WriteLine($"Есть хотя бы один отрицательный: {естьОтрицательный}");
+        Console.ReadKey();
     }
 }
