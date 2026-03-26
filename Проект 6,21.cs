49b3a893fd83577d227f00213e9c594aeb5f318e
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:20:59 +0400

    Update Проект 6,21.cs

--- Проект 6,21.cs
+++ Проект 6,21.cs
@@ -1,32 +1,30 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите длину массива n: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] массив = new int[n];
+
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("Введите длину массива n: ");
-            int n = int.Parse(Console.ReadLine());      
-            int[] массив = new int[n];
-            
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            } 
-            Console.Write("Введите количество сдвигов m: ");
-            int m = int.Parse(Console.ReadLine());
-            m = m % n;
-            int[] сдвинутый = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                сдвинутый[(i + m) % n] = массив[i];
-            }
-            Console.WriteLine("Массив после циклического сдвига:");
-            foreach (int элемент in сдвинутый)
-            {
-                Console.Write(элемент + " ");
-            }
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        Console.Write("Введите количество сдвигов m: ");
+        int m = int.Parse(Console.ReadLine());
+        m = m % n;
+        int[] сдвинутый = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            сдвинутый[(i + m) % n] = массив[i];
+        }
+        Console.WriteLine("Массив после циклического сдвига:");
+        foreach (int элемент in сдвинутый)
+        {
+            Console.Write(элемент + " ");
+        }
+        Console.ReadKey();
     }
 }
