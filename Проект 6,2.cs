402a36329756d2ec5bf60d46a4f73cc1b2e55af6
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:20:04 +0400

    Update Проект 6,2.cs

--- Проект 6,2.cs
+++ Проект 6,2.cs
@@ -1,28 +1,26 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        double[] массив = new double[8];
+        for (int i = 0; i < массив.Length; i++)
         {
-            double[] массив = new double[8];            
-            for (int i = 0; i < массив.Length; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = double.Parse(Console.ReadLine());
-            }           
-            double сумма = 0;
-            int количество = 0;          
-            foreach (double элемент in массив)
-            {
-                if (элемент > 0)
-                {
-                    сумма += элемент;
-                    количество++;
-                }
-            }         
-            double среднее = сумма / количество;
-            Console.WriteLine($"Среднее арифметическое положительных элементов: {среднее:F2}");
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = double.Parse(Console.ReadLine());
         }
+        double сумма = 0;
+        int количество = 0;
+        foreach (double элемент in массив)
+        {
+            if (элемент > 0)
+            {
+                сумма += элемент;
+                количество++;
+            }
+        }
+        double среднее = сумма / количество;
+        Console.WriteLine($"Среднее арифметическое положительных элементов: {среднее:F2}");
+        Console.ReadKey();
     }
 }
