afa7afc7485f043629007dae54d08f1c8edd87bc
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:22:09 +0400

    Update Проект 6,22.2.cs

--- Проект 6,22.2.cs
+++ Проект 6,22.2.cs
@@ -1,38 +1,36 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите длину массива n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] массив = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("Введите длину массива n: ");
-            int n = int.Parse(Console.ReadLine());
-            
-            int[] массив = new int[n];   
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            }
-            for (int i = 0; i < n - 1; i++)
-            {
-                int минИндекс = i;
-                for (int j = i + 1; j < n; j++)
-                {
-                    if (массив[j] < массив[минИндекс])
-                    {
-                        минИндекс = j;
-                    }
-                }
-                int temp = массив[i];
-                массив[i] = массив[минИндекс];
-                массив[минИндекс] = temp;
-            }
-            Console.WriteLine("Отсортированный массив (выбором):");
-            foreach (int элемент in массив)
-            {
-                Console.Write(элемент + " ");
-            }
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        for (int i = 0; i < n - 1; i++)
+        {
+            int минИндекс = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (массив[j] < массив[минИндекс])
+                {
+                    минИндекс = j;
+                }
+            }
+            int temp = массив[i];
+            массив[i] = массив[минИндекс];
+            массив[минИндекс] = temp;
+        }
+        Console.WriteLine("Отсортированный массив (выбором):");
+        foreach (int элемент in массив)
+        {
+            Console.Write(элемент + " ");
+        }
+        Console.ReadKey();
     }
 }
