82e70098c9d51d678afb3be3892255897a6bcc5f
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:17:40 +0400

    Update Проект 6,16.cs

--- Проект 6,16.cs
+++ Проект 6,16.cs
@@ -1,39 +1,37 @@
 using System;
 using System.Collections.Generic;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите длину массива n: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] массив = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("Введите длину массива n: ");
-            int n = int.Parse(Console.ReadLine());       
-            int[] массив = new int[n];    
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            }   
-            List<int> новыйСписок = new List<int>();
-            bool начатьКопировать = false;
-            
-            foreach (int элемент in массив)
-            {
-                if (!начатьКопировать && элемент >= 20)
-                {
-                    начатьКопировать = true;
-                }
-                if (начатьКопировать)
-                {
-                    новыйСписок.Add(элемент);
-                }
-            }
-            int[] новыйМассив = новыйСписок.ToArray();
-            Console.WriteLine("Новый массив:");
-            foreach (int элемент in новыйМассив)
-            {
-                Console.Write(элемент + " ");
-            }
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        List<int> новыйСписок = new List<int>();
+        bool начатьКопировать = false;
+
+        foreach (int элемент in массив)
+        {
+            if (!начатьКопировать && элемент >= 20)
+            {
+                начатьКопировать = true;
+            }
+            if (начатьКопировать)
+            {
+                новыйСписок.Add(элемент);
+            }
+        }
+        int[] новыйМассив = новыйСписок.ToArray();
+        Console.WriteLine("Новый массив:");
+        foreach (int элемент in новыйМассив)
+        {
+            Console.Write(элемент + " ");
+        }
+        Console.ReadKey();
     }
 }
