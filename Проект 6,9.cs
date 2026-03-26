cd69c8d91f4083bfe6aaf296f5f62099e05873ab
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:28:51 +0400

    Update Проект 6,9.cs

--- Проект 6,9.cs
+++ Проект 6,9.cs
@@ -1,28 +1,26 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите длину массива n: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] массив = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("Введите длину массива n: ");
-            int n = int.Parse(Console.ReadLine());            
-            int[] массив = new int[n];          
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Введите элемент {i + 1}: ");
-                массив[i] = int.Parse(Console.ReadLine());
-            }    
-            bool всеЧетные = true;
-            foreach (int элемент in массив)
-            {
-                if (элемент % 2 != 0)
-                {
-                    всеЧетные = false;
-                    break;
-                }
-            }  
-            Console.WriteLine($"Все элементы четные: {всеЧетные}");
-            Console.ReadKey();
+            Console.Write($"Введите элемент {i + 1}: ");
+            массив[i] = int.Parse(Console.ReadLine());
         }
+        bool всеЧетные = true;
+        foreach (int элемент in массив)
+        {
+            if (элемент % 2 != 0)
+            {
+                всеЧетные = false;
+                break;
+            }
+        }
+        Console.WriteLine($"Все элементы четные: {всеЧетные}");
+        Console.ReadKey();
     }
 }
