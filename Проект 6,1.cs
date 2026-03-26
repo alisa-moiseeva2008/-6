2eed3b491a51f5e9218a70a68547ae649dfb1e89
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:14:05 +0400

    Update Проект 6,1.cs

--- Проект 6,1.cs
+++ Проект 6,1.cs
@@ -1,23 +1,21 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите число M: ");
+        int M = int.Parse(Console.ReadLine());
+        int[] массив = new int[10];
+
+        for (int i = 0; i < массив.Length; i++)
         {
-            Console.Write("Введите число M: ");
-            int M = int.Parse(Console.ReadLine());            
-            int[] массив = new int[10];
-            
-            for (int i = 0; i < массив.Length; i++)
-            {
-                массив[i] = M;
-            }           
-            Console.WriteLine("Массив:");
-            foreach (int элемент in массив)
-            {
-                Console.Write(элемент + " ");
-            }
-            Console.ReadKey();
+            массив[i] = M;
         }
+        Console.WriteLine("Массив:");
+        foreach (int элемент in массив)
+        {
+            Console.Write(элемент + " ");
+        }
+        Console.ReadKey();
     }
 }
